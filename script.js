b6f6860d461bbea2656d171406f4ca0e64e0246f
Mindless-Magnet <muhammedshajahan45@gmail.com>
2023-04-20 10:00:31 +0530

    added classes and and linked it to js

--- script.js
+++ script.js
@@ -40,3 +40,24 @@ function operate(a, b, o){
     };
 }
 
+// getting the buttons
+const num0 = document.querySelector('.0');
+const num1 = document.querySelector('.1');
+const num2 = document.querySelector('.2');
+const num3 = document.querySelector('.3');
+const num4 = document.querySelector('.4');
+const num5 = document.querySelector('.5');
+const num6 = document.querySelector('.6');
+const num7 = document.querySelector('.7');
+const num8 = document.querySelector('.8');
+const num9 = document.querySelector('.9');
+
+const decimal = document.querySelector('.decimal');
+
+const equals = document.querySelector('.equals');
+const plus = document.querySelector('.plus');
+const minus = document.querySelector('.minus');
+const divide = document.querySelector('.divide');
+const modulus = document.querySelector('.modulus');
+const multiply = document.querySelector('.multiply');
+
